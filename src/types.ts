524d32f61d3b0b7831a97b89572df66d2eafb976
Erick244 <erienrique03@gmail.com>
2025-06-20 09:54:00 -0300

    feat: add baileysOptions to FiWhatsAppOptions
    This commit adds the `baileysOptions` property to the `FiWhatsAppOptions` interface. This allows users to pass custom options to the underlying Baileys socket.

--- src/types.ts
+++ src/types.ts
@@ -1,33 +1,34 @@
 import type {
-  Browsers,
-  WAMessage,
-  WAMessageUpdate,
-  BaileysEventMap,
+    BaileysEventMap,
+    Browsers,
+    UserFacingSocketConfig,
+    WAMessage,
 } from "baileys";
 
 export interface FiWhatsAppOptions {
-  logPath?: string;
-  sessionDir?: string;
-  maxRetries?: number;
-  browser?: keyof typeof Browsers;
-  device?: string;
-  phoneNumber?: string;
-  mongodb?: {
-    url: string;
-    databaseName?: string;
-    collectionName?: string;
-  };
+    logPath?: string;
+    sessionDir?: string;
+    maxRetries?: number;
+    browser?: keyof typeof Browsers;
+    device?: string;
+    phoneNumber?: string;
+    mongodb?: {
+        url: string;
+        databaseName?: string;
+        collectionName?: string;
+    };
+    baileysOptions?: UserFacingSocketConfig;
 }
 
 export interface FiWhatsAppEventMap {
-  qr: [string];
-  pairingCode: [string];
-  ready: [];
-  reconnect: [];
-  logout: [];
-  error: [Error];
-  message: [WAMessage];
-  messageFromClient: [WAMessage];
-  "messages.delete": [BaileysEventMap["messages.delete"]];
-  "messages.update": [BaileysEventMap["messages.update"]];
+    qr: [string];
+    pairingCode: [string];
+    ready: [];
+    reconnect: [];
+    logout: [];
+    error: [Error];
+    message: [WAMessage];
+    messageFromClient: [WAMessage];
+    "messages.delete": [BaileysEventMap["messages.delete"]];
+    "messages.update": [BaileysEventMap["messages.update"]];
 }
